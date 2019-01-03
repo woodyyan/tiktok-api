df673bfe372fc6e6a729402d4cfdd20a158b6e77
Songbai Yan <colorguitar@hotmail.com>
2019-01-03 14:25:36 +0800

    first commit

---
+++ src/main/java/com/daduo/api/tiktokapi/TiktokApiApplication.java
@@ -0,0 +1,14 @@
+package com.daduo.api.tiktokapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokApiApplication.class, args);
+	}
+
+}
+
